4f25a554474e265cdf45a9e4c229b2876b406a1b
Anton Kruglyakov <akrugs@mail.ru>
2023-03-11 01:28:51 +0500

    Задача 10

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//*/
+// Задача 10
+Console.Write("Введите число c тремя и более разрядами: ");
+string num = Console.ReadLine()!;
+int number = int.Parse(num);
+if (num.Length >= 3 && number>=0) {
+    Console.WriteLine($"{num}->{num[1]}");
+} else if(num.Length >= 4 && number < 0) {
+    Console.Write($"{num}->{num[2]}");
+} else {
+    Console.Write("Введено неверное число!");
+}
+//*/
