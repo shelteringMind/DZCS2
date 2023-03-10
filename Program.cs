528fae8330a0af98fb53230ff11a0dfa32e3f3ce
Anton Kruglyakov <akrugs@mail.ru>
2023-03-11 01:31:07 +0500

    Задача 15

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ if (num.Length >= 3 && number>=0) {
 }
 //*/
 
-//*/
+/*/
 // Задача 13
 Console.Write("Введите число: ");
 string num = Console.ReadLine()!;
@@ -23,5 +23,23 @@ if (num.Length >= 3 && number>=0) {
     Console.Write($"{num}->{num[3]}");
 } else {
     Console.Write("Третьей цифры нет!");
+}
+//*/
+
+//*/
+// Задача 15
+Console.Write("Введите номер дня недели для проверки того,\nявляется ли он выходным: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number>=1 && number<=7) {
+    switch(number) {
+        case 1 or 2 or 3 or 4 or 5:
+        Console.Write("Нет, это будний день!");
+        break;
+        case 6 or 7:
+        Console.Write("Да, этот день выходной!");
+        break;
+    }
+} else {
+    Console.Write("Такого дня недели не существует!");
 }
 //*/
